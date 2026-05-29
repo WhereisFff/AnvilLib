f6e970bfdaa12c3e37b978cf77e72f7adf83cf72
Gugle <administration@ztxy666.cn>
2026-05-30 03:54:38 +0800

    feat(piston): enhance PistonBaseBlockMixin with shared block entity handling

---
+++ module.moveable-entity-block/src/main/java/dev/anvilcraft/lib/v2/piston/AnvilLibMoveableEntityBlock.java
@@ -0,0 +1,9 @@
+package dev.anvilcraft.lib.v2.piston;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class AnvilLibMoveableEntityBlock {
+    public static final String MAIN_ID = "anvillib";
+    public static final Logger LOGGER = LogUtils.getLogger();
+}
